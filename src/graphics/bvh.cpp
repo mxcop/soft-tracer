8720910ccb2fcf771d69766a8b8db4f27612ab92
MΛX <mxcop.dev@gmail.com>
2024-01-28 17:48:13 +0100

    feat: single voxel!

--- src/graphics/bvh.cpp
+++ src/graphics/bvh.cpp
@@ -206,83 +206,56 @@ f32 Bvh::intersect(const Ray& ray) const {
             /* Check if we hit any primitives */
             for (u32 i = 0; i < node->prim_count; ++i) {
                 const VoxelVolume& prim = prims[node->left_first + i];
-                glm::vec2 dist = ray.intersection_aabb_sse(prim.aabb_min4, prim.aabb_max4);
+                f32 dist = ray.intersects_aabb_sse(prim.aabb_min4, prim.aabb_max4);
 
-/* Hit occured */
-#if 1
-                if (dist.x < BIG_F32) {
+                /* Hit occured */
+                if (dist < BIG_F32) {
                     /* Get the origin of the ray in voxel space */
                     // glm::vec3 ray_pos =
                     //     ((ray.origin + ray.dir * dist) - prim.aabb_min);
                     // glm::ivec3 vox_pos =
                     //     glm::ivec3(floor(ray_pos.x), floor(ray_pos.y), floor(ray_pos.z));
+                    const glm::vec3 prim_size = (prim.aabb_max - prim.aabb_min) * VOXELS_PER_UNIT;
 
-                    const f32 tmin = dist.x, tmax = dist.y;
+                    /* Move up to the edge of the bounding box */
+                    glm::vec3 p = ray.origin + ray.dir * dist;
 
-                    const glm::vec3 ray_start = (ray.origin + ray.dir * tmin);
-                    const glm::vec3 ray_end = (ray.origin + ray.dir * tmax);
+                    /* Voxel position */
+                    glm::vec3 vp = (p - prim.aabb_min) * VOXELS_PER_UNIT;
+                    glm::vec3 idx = glm::floor(vp);
 
-                    glm::ivec3 vox_pos = glm::floor((ray_start - prim.aabb_min) * VOXELS_PER_UNIT);
-                    const glm::ivec3 vox_end =
-                        glm::floor((ray_end - prim.aabb_min) * VOXELS_PER_UNIT);
+                    /* Ray direction sign mask */
+                    glm::vec3 srd = glm::sign(ray.dir);
+                    glm::vec3 sd = ((idx - vp) + (srd * .5f) + .5f) * ray.inv_dir;
 
-                    glm::ivec3 ray_step;
-                    glm::vec3 t_delta;
-                    glm::vec3 t_max;
-                    for (int i = 0; i < 3; i++) {
-                        if (ray.dir[i] > 0.0f) {
-                            ray_step[i] = 1;
-                            t_delta[i] = VOXELS_PER_UNIT / ray.dir[i];
-                            t_max[i] = tmin + (prim.aabb_min[i] + vox_pos[i] * VOXELS_PER_UNIT -
-                                               ray_start[i]) /
-                                                  ray.dir[i];
-                        } else if (ray.dir[i] < 0.0f) {
-                            ray_step[i] = -1;
-                            t_delta[i] = VOXELS_PER_UNIT / -ray.dir[i];
-                            const size_t pre_idx = vox_pos[i] - 1;
-                            t_max[i] = tmin + (prim.aabb_min[i] + pre_idx * VOXELS_PER_UNIT -
-                                               ray_start[i]) /
-                                                  ray.dir[i];
-                        } else {
-                            ray_step[i] = 0;
-                            t_delta[i] = tmax;
-                            t_max[i] = tmax;
-                        }
-                    }
+                    for (int i = 0; i < 128; ++i) {
+                        /* Index the voxel data texture */
+                        // float voxel = fetch_voxel(idx);
 
-                    int si = 0;
-                    bool hit = false;
-                    while (si < 200 && vox_pos.x != vox_end.x) {
-                        if (vox_pos == glm::ivec3(1)) {
-                            glm::vec3 final_pos =
-                                glm::vec3(vox_pos) * VOXELS_PER_UNIT + prim.aabb_min;
-                            dist.x = glm::distance(final_pos, ray.origin);
-                            hit = true;
+                        if (idx == glm::vec3(1.0)) {
+                            dist = 100.0f;
                             break;
                         }
 
-                        if (t_max.x < t_max.y && t_max.x < t_max.z) {
-                            // X-axis traversal.
-                            vox_pos.x += ray_step.x;
-                            t_max.x += t_delta.x;
-                        } else if (t_max.y < t_max.z) {
-                            // Y-axis traversal.
-                            vox_pos.y += ray_step.y;
-                            t_max.y += t_delta.y;
-                        } else {
-                            // Z-axis traversal.
-                            vox_pos.z += ray_step.z;
-                            t_max.z += t_delta.z;
+                        /* Compute the step mask */
+                        glm::vec3 yzx = sd.yzx, zxy = sd.zxy;
+                        glm::vec3 mask = glm::lessThanEqual(sd, glm::min(yzx, zxy));
+                        // vec3 mask = vec3(lessThanEqual(sd.xyz, min(sd.yzx, sd.zxy)));
+
+                        /* Step to the next voxel */
+                        sd += mask * srd * ray.inv_dir;
+                        idx += mask * srd;
+
+                        /* Check if we're still within the bounding volume */
+                        if (glm::any(glm::lessThan(idx, glm::vec3(0))) ||
+                            glm::any(glm::greaterThanEqual(idx, prim_size))) {
+                            dist = 10000.0f;
+                            break;
                         }
-                        si++;
                     }
-                    if (not hit) dist.x = BIG_F32;
-
-                    // dist = (vox_pos.x % 2 == 0) ? 1000.0f : dist;
                 }
-#endif
 
-                mind = std::min(dist.x, mind);
+                mind = std::min(dist, mind);
             }
             // if (mind < BIG_F32) return mind;
 
